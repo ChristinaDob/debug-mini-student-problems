88c4e5372baa73d6a81829215145a34f41f91214
Christina Dob <cd@cd.local>
2019-03-27 11:47:50 -0700

    finished probelm 4

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -15,34 +15,40 @@ export default class MyComponent extends Component {
 
   fetchData() {
     this.setState({ isPosting: true, error: null });
-    axios.get('https://swapi.co/api/people').then(response => {
-      this.setState({ data: response.data });
-    }).catch(error => {
-      this.setState({ error });
-    }).then(() => {
-      this.setState({ isPosting: false });
-    });
+    axios
+      .get('https://swapi.co/api/people')
+      .then(response => {
+        console.log(response.data);
+        this.setState({ data: response.data.results });
+      })
+      .catch(error => {
+        this.setState({ error });
+      })
+      .then(() => {
+        this.setState({ isPosting: false });
+      });
   }
 
   render() {
     const { data, error, isPosting } = this.state;
-    
+
     return (
       <div className="my-component-container">
         <h2>My Component</h2>
         <button onClick={this.fetchData}>Fetch data</button>
-        {isPosting
-          ? <div>Loading...</div>
-          : error
-            ? <div>Error! {error.message}</div>
-            : !data.length
-              ? <div>No items in list</div>
-              : <div>
-                {data.map(x => <div>
-                  Name: {x.name}
-                </div>)}
-              </div>
-        }
+        {isPosting ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error! {error.message}</div>
+        ) : !data.length ? (
+          <div>No items in list</div>
+        ) : (
+          <div>
+            {data.map(x => (
+              <div>Name: {x.name}</div>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
